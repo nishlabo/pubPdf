6017dd8b556104a994d5b5c85b62882b2e7e0104
nishlabo <128766256+nishlabo@users.noreply.github.com>
2024-01-10 09:26:25 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pubPdf',
+    version='0.1.0',
+    url='https://github.com/nishlabo/pubPdf.git',
+    author='nishlabo',
+    author_email='n.ishiharajp@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=['reportlab', 'svglib'],
+)
